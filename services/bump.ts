8283ed5d4a02a2caf8a6e62a9d5b9cbdbdd595af
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-02-13 08:36:18 +0530

    new

--- services/bump.ts
+++
@@ -1,8 +0,0 @@
-import { PublicKey, Keypair } from "@solana/web3.js";
-import bs58 from 'bs58';
-
-export const startBump = async (walletPrivate: string, txn_amount: number, tokenAddress: string) => {
-    const keypair = Keypair.fromSecretKey(bs58.decode(walletPrivate));
-    const mint = new PublicKey(tokenAddress);
-    console.log(keypair.publicKey.toString(), txn_amount, mint);
-}
